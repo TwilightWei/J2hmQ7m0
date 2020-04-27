b1871673d2356ef7985b19877059842ce52562b1
TwilightWei <tom56092002@gmail.com>
2020-04-27 22:11:12 +0800

    Implement get().

--- x_1/app/config.py
+++ x_1/app/config.py
@@ -1,37 +1,40 @@
 import json
 
 class Config():
-    _config_dir = None
-    _config_data = None
+    def __init__(self):
+        self._config_dir = "app/config.json"
+        self._config_data = None
+        with open(self._config_dir, 'r') as json_file:
+            self._config_data = json.load(json_file)
 
     def get_property(self, property_name):
-        pass
+        return self._config_data[property_name]
 
 class MyViewConfig(Config):
     def get_folder_dir(self):
-        pass
+        return self.get_property("folder_dir")
     
     def get_csv_name(self):
-        pass
+        return self.get_property("csv_name")
 
 class CustomerConfig(Config):
     def get_id_length(self):
-        pass
+        return self.get_property("id_length")
 
     def get_name_number(self):
-        pass
+        return self.get_property("name_number")
 
     def get_name_list(self):
-        pass
+        return self.get_property("name_list")
 
     def get_country_code(self):
-        pass
+        return self.get_property("country_code")
 
     def get_door_number(self):
-        pass
+        return self.get_property("door_number")
 
     def get_phone_length(self):
-        pass
+        return self.get_property("phone_length")
 
     def get_frequency_range(self):
-        pass
+        return self.get_property("frequency_range")
--- x_1/app/views.py
+++ x_1/app/views.py
@@ -1,15 +1,31 @@
+import os
+
 from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.views import View
 
+from app.config import MyViewConfig
+from app.customer import Customer
+
 # Create your views here.
 class MyView(View):
-    - _config = None
+    _config = None
 
     def get(request):
+        response = "Folder existed"
 
-        return HttpResponse('get')
+        if request.method == 'GET':
+            config = MyViewConfig()
+            folder_dir = config.get_folder_dir()
+            if not os.path.isdir(folder_dir):
+                os.mkdir(folder_dir)
+                response = "Folder created"
+            return HttpResponse(response)
+        
+        else:
+            response = "Request id not GET!"
+            return HttpResponse(response)
     
     def create_csv(request):
 
